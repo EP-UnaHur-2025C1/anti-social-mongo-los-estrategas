06d5966555f2d4e9d2102d9f739cbd0d04445cfc
ERIKA <erika@kali.erikacampos.com>
2025-06-20 15:08:11 -0300

    feature swagger.yalm

--- src/main.js
+++ src/main.js
@@ -20,3 +20,8 @@ app.listen(PORT, () => {
   console.log(`Servidor corriendo en puerto ${PORT}`);
 });
 
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./docs/swagger.yaml');
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
