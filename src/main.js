8d49237cc43e4ee5526215a2b263dc916df4da39
ERIKA <erika@kali.erikacampos.com>
2025-06-20 12:41:45 -0300

    feature bd.js y main.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,22 @@
-console.log("UnaHur - Anti-Social net");
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+//Middleware
+app.use(express.json());
+
+// Rutas
+app.use('/api/users', require('./routes/user.routes'));
+
+//Conexion a MongoDB
+connectDB();
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
+});
+
