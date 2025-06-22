23de8e8f20c4765f146039f59863a325cafba042
juancamargo02 <juaninachocamargo@hotmail.com>
2025-06-22 16:54:27 -0300

    Codigo de swagger en el main borrado, se accede a la documentacion con el archivo. Error de tipeo en model/post corregido

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const path = require('path');
 const connectDB = require('./config/db');
 
 dotenv.config();
@@ -20,19 +19,11 @@ const postRoutes = require("./routes/postRoutes")
 const tagRoutes = require("./routes/tagRoutes")
 
 // Rutas
-app.use('/api/users', userRoutes);
 app.use('/Users', userRoutes);
 app.use('/Comments', commentRoutes);
 app.use('/Posts', postRoutes);
 app.use('/Tags', tagRoutes);
 
-
-
-const swaggerUi = require('swagger-ui-express');
-const YAML = require('yamljs');
-const swaggerDocument = YAML.load(path.join(__dirname, '../docs/swagger_documentation.yaml'));
-
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en puerto ${PORT}`);
--- src/models/post.js
+++ src/models/post.js
@@ -4,7 +4,7 @@ const PostSchema = new mongoose.Schema({
   textoPost: {
     type: String,
     required: [true, "El post es obligatorio"],
-    minlegth: [1, "El post no puede estar vacío"],
+    minlength: [1, "El post no puede estar vacío"],
     maxlength: [300, "El post debe tener menos de 300 caracteres"]
   },
   user: { // autor del post
